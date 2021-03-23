5221baccfd82c586dd7f2e0a12c551ac523a98a6
wangxiaokang <1196169400@qq.com>
2021-03-23 22:21:34 +0800

    n

---
+++ 4.mystl/basic_string.h
@@ -0,0 +1,77 @@
+
+
+namespace mystl
+{
+
+template<typename CharType>
+struct char_traits
+{
+    typedef CharType typechar;
+
+    static size_t length(const typechar* str)
+    {
+        typechar* p = str;
+        size_t num = 0;
+        while(p++) {
+            num++;
+        }
+    }
+
+    static int compare(const typechar* str1, const typechar* str2)
+    {
+        typechar* p1 = str1, *p2 = str2;
+        while (p1 != nullptr && p2 != nullptr) {
+            if (*p1 > *p2)
+                return 1;
+            if (*p1 < *p2)
+                return -1;
+        }
+        if (p1 != nullptr)
+            return 1;
+        if (p2 != nullptr)
+            return -1;
+        return 0;
+    }
+
+    static typechar* copy(typechar* src, typechar* dst, size_t n)
+    {
+        assert(src + n <= dst || dst + n <= src);
+        typechar *r = dst;
+        while (n--) {
+            *dst++ = *src++;
+        }
+    }
+
+    static typechar* move(typechar* src, typechar* dst,size_t n)
+    {
+        typechar* r = dst;
+        if (src == dst)
+            return ;
+        if (src < dst && src + n > dst) {
+            src += n-1;
+            dst += n-1;
+            while (n--)
+                *dst-- = *src--;
+        } else if {
+            while (n--)
+                *dst++ = *src++;
+        }
+        return r;
+    }
+    
+    static typechar* fill(typechar* dst, typechar value, size_t count)
+    {
+        typechar* r = dst;
+        while (count--) {
+            *dst++ = value;
+        }
+        return r;
+    }
+}
+
+template<typename >
+
+
+
+
+}
